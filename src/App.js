51183a9561cb892a9254746c39bea70569a615ab
hanamantk <hanmant2011@hotmail.com>
2018-11-17 13:01:16 +0530

    fist commit

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './App.css';
+import Remote from './remote';
+
+const matrix=[[1,2,3,4],
+              [5,6,7,8],
+              [9,10,11,12],
+              [13,14,15,16],
+              [17,18,19,20]
+          ]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+                mat:matrix,
+                position:'22'  
+              };
+              this.highlightCell=this.highlightCell.bind(this);
+  }
+
+  getNextCell(row,rowOrCol){
+    this.currntPosition=rowOrCol==='row'?(row+this.currntPosition[1]):
+                                (this.currntPosition[0]+row);
+     var render= this.getBoundaryOfTable(this.currntPosition);
+    var isRender=(render===true)?this.setState({position:this.currntPosition}):null; 
+
+  }
+
+  getRow(self){
+    this.currntPosition=self.state.position;
+    var row=parseInt(this.currntPosition[0]);
+    return row;
+  }
+
+  getColumn(self){
+    this.currntPosition=self.state.position;
+    var col=parseInt(this.currntPosition[1]);
+    return col;
+  }
+
+  highlightCell(action){
+    var self=this;
+    switch(action){
+      case 'up':  var row=this.getRow(self);
+                    row=row-1;
+                    this.getNextCell(row,'row');
+                     
+                   break;
+      case 'down' : var row=this.getRow(self);
+                    row=row+1;
+                    this.getNextCell(row,'row');
+                    break;
+      case 'left' : var col=this.getColumn(self);
+                        col=col-1;
+                        this.getNextCell(col,'col');
+                    break;
+      case 'right':var col=this.getColumn(self);
+                    col=col+1;
+                    this.getNextCell(col,'col');
+                    break;
+      default     :   break;
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    
+    return this.getBoundaryOfTable(nextState.position);
+  }
+
+  getBoundaryOfTable(nextState){
+    var columnLength=this.state.mat[0].length || 0;
+    if(nextState[1]>(columnLength-1) || nextState.length>=3 ){
+      return false;
+    }else if(nextState[0]>(columnLength)){
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+     var position=this.state.position;
+      var myTable=this.state.mat.map((row,i)=>{
+             var columns=row.map((elm,j)=>{
+                  return (<td key={j} className={((''+i+j)===position)?'cell':null}>{''+i+j}</td>);
+                });
+                return (<tr key={i}>{columns}</tr>)
+        
+      });
+  
+   return (
+      <div className="App">
+        <table  id="matrix">
+          <tbody>{myTable}</tbody>
+       </table>
+       <Remote highlight={this.highlightCell}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/remote.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+class Remote extends Component {
+  constructor(props) {
+    super(props);
+    this.callPar=this.callPar.bind(this);
+  }
+callPar(action){
+    this.props.highlight(action)
+}
+
+  render() {
+      
+   return (
+      <div className="remote">
+        <div><button onClick={this.callPar.bind(this,'up')}> ^</button></div>
+            <div className="midbtn">
+                <span className="lftbtn"><button onClick={this.callPar.bind(this,'left')} >&lt;</button></span>
+                <span><button className="rtbtn" onClick={this.callPar.bind(this,'right')}> ></button></span>
+            </div>
+          
+            <div className="clrfix downbtn "><button onClick={this.callPar.bind(this,'down')}>v</button></div>
+      </div>
+     
+    );
+  }
+}
+
+export default Remote;
